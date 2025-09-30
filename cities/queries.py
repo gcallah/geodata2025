ff935302916f2c3588bef521e629e8185c4b78f7
gcallah <gcallah@mac.com>
2025-09-30 12:08:25 -0400

    Requiring name for a city.

--- cities/queries.py
+++ cities/queries.py
@@ -3,7 +3,21 @@
 This file deals with our city-level data.
 """
 
+SUCCESS = 0
+NAME = 'name'
+STATE_CODE = 'state_code'
 
-def create(name: str):
-    if not isinstance(name, str):
-        raise ValueError(f'Bad type for {type(name)=}')
+cities = {}
+
+SAMPLE_CITY = {
+    NAME: 'New York',
+    STATE_CODE: 'NY',
+}
+
+
+def create(flds: str):
+    if not isinstance(flds, dict):
+        raise ValueError(f'Bad type for {type(flds)=}')
+    if not flds.get(NAME):
+        raise ValueError(f'Bad value for {flds.get(NAME)=}')
+    return SUCCESS
--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -3,6 +3,10 @@ import pytest
 import cities.queries as qry
 
 
-def test_create():
+def test_good_create():
+    assert qry.create(qry.SAMPLE_CITY) == qry.SUCCESS
+
+
+def test_create_bad_param_type():
     with pytest.raises(ValueError):
         qry.create(17)
