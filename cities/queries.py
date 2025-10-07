cc4688a7d3564fbe31119e4d1f77170cc983affd
gcallah <gcallah@mac.com>
2025-10-07 15:06:55 -0400

    Add cities read.

--- cities/queries.py
+++ cities/queries.py
@@ -2,6 +2,8 @@
 """
 This file deals with our city-level data.
 """
+from random import randint
+
 
 MIN_ID_LEN = 1
 
@@ -9,12 +11,21 @@ ID = 'id'
 NAME = 'name'
 STATE_CODE = 'state_code'
 
-city_cache = {}
-
 SAMPLE_CITY = {
     NAME: 'New York',
     STATE_CODE: 'NY',
 }
+
+city_cache = {
+    1: SAMPLE_CITY,
+}
+
+
+def db_connect(success_ratio: int) -> bool:
+    """
+    Return True if connected, False if not.
+    """
+    return randint(1, success_ratio) % success_ratio
 
 
 def is_valid_id(_id: str) -> bool:
@@ -29,7 +40,7 @@ def num_cities() -> int:
     return len(city_cache)
 
 
-def create(flds: str):
+def create(flds: dict) -> str:
     if not isinstance(flds, dict):
         raise ValueError(f'Bad type for {type(flds)=}')
     if not flds.get(NAME):
@@ -37,3 +48,17 @@ def create(flds: str):
     new_id = str(len(city_cache) + 1)
     city_cache[new_id] = flds
     return new_id
+
+
+def read() -> dict:
+    if not db_connect(3):
+        raise ConnectionError('Could not connec to DB.')
+    return city_cache
+
+
+def main():
+    print(read())
+
+
+if __name__ == '__main__':
+    main()
--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import pytest
 
 import cities.queries as qry
@@ -5,20 +6,22 @@ import cities.queries as qry
 
 @pytest.mark.skip('This is an example of a bad test!')
 def test_bad_test_for_num_cities():
-    assert qry.num_cities() == len(qry.cities)
+    assert qry.num_cities() == len(qry.city_cache)
 
 
 def test_num_cities():
+    # get the count
     old_count = qry.num_cities()
+    # add a record
     qry.create(qry.SAMPLE_CITY)
-    assert qry.num_cities() > old_count
+    assert qry.num_cities() == old_count + 1
 
 
 def test_good_create():
     old_count = qry.num_cities()
     new_rec_id = qry.create(qry.SAMPLE_CITY)
     assert qry.is_valid_id(new_rec_id)
-    assert qry.num_cities() > old_count
+    assert qry.num_cities() == old_count + 1
 
 
 def test_create_bad_name():
@@ -29,3 +32,10 @@ def test_create_bad_name():
 def test_create_bad_param_type():
     with pytest.raises(ValueError):
         qry.create(17)
+
+
+@patch('cities.queries.db_connect', return_value=True)
+def test_read(mock_db_connect):
+    cities = qry.read()
+    assert isinstance(cities, dict)
+    assert len(cities) > 1
