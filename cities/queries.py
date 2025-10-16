e3a6a989d7be9add0c4955eea4b688081d86c711
gcallah <gcallah@mac.com>
2025-10-16 12:03:14 -0400

    Adding delete to cities query.

--- cities/queries.py
+++ cities/queries.py
@@ -16,9 +16,7 @@ SAMPLE_CITY = {
     STATE_CODE: 'NY',
 }
 
-city_cache = {
-    1: SAMPLE_CITY,
-}
+city_cache = {}
 
 
 def db_connect(success_ratio: int) -> bool:
@@ -48,6 +46,13 @@ def create(flds: dict) -> str:
     new_id = str(len(city_cache) + 1)
     city_cache[new_id] = flds
     return new_id
+
+
+def delete(city_id: str) -> bool:
+    if city_id not in city_cache:
+        raise ValueError(f'No such city: {city_id}')
+    del city_cache[city_id]
+    return True
 
 
 def read() -> dict:
--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -4,6 +4,12 @@ import pytest
 import cities.queries as qry
 
 
+@pytest.fixture(scope='function')
+def temp_city():
+    new_rec_id = qry.create(qry.SAMPLE_CITY)
+    yield new_rec_id
+
+
 @pytest.mark.skip('This is an example of a bad test!')
 def test_bad_test_for_num_cities():
     assert qry.num_cities() == len(qry.city_cache)
@@ -35,14 +41,25 @@ def test_create_bad_param_type():
 
 
 @patch('cities.queries.db_connect', return_value=True, autospec=True)
-def test_read(mock_db_connect):
-    new_rec_id = qry.create(qry.SAMPLE_CITY)
+def test_delete(mock_db_connect, temp_city):
+    qry.delete(temp_city)
+    assert temp_city not in qry.read()
+
+
+@patch('cities.queries.db_connect', return_value=True, autospec=True)
+def test_delete_not_there(mock_db_connect):
+    with pytest.raises(ValueError):
+        qry.delete('some value that is not there')
+
+
+@patch('cities.queries.db_connect', return_value=True, autospec=True)
+def test_read(mock_db_connect, temp_city):
     cities = qry.read()
     assert isinstance(cities, dict)
-    assert len(cities) > 1
+    assert temp_city in cities
 
 
 @patch('cities.queries.db_connect', return_value=False, autospec=True)
-def test_read(mock_db_connect):
+def test_read_cant_connect(mock_db_connect):
     with pytest.raises(ConnectionError):
         cities = qry.read()
