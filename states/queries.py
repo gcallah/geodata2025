ad494f9d39c264b4814c5c9d2e41d8bbcfd4b83b
gcallah <gcallah@mac.com>
2025-11-20 11:28:42 -0500

    Adding state query module to repo.

---
+++ states/queries.py
@@ -0,0 +1,90 @@
+
+"""
+This file deals with our state-level data.
+"""
+from functools import wraps
+
+import data.db_connect as dbc
+from data.db_connect import is_valid_id  # noqa F401
+
+STATE_COLLECTION = 'states'
+
+ID = 'id'
+NAME = 'name'
+CODE = 'code'
+COUNTRY_CODE = 'country_code'
+
+SAMPLE_CODE = 'NY'
+SAMPLE_COUNTRY = 'USA'
+SAMPLE_KEY = (SAMPLE_CODE, SAMPLE_COUNTRY)
+SAMPLE_STATE = {
+    NAME: 'New York',
+    CODE: SAMPLE_CODE,
+    COUNTRY_CODE: SAMPLE_COUNTRY,
+}
+
+cache = None
+
+
+def needs_cache(fn, *args, **kwargs):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if not cache:
+            load_cache()
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@needs_cache
+def count() -> int:
+    return len(cache)
+
+
+@needs_cache
+def create(flds: dict) -> str:
+    if not isinstance(flds, dict):
+        raise ValueError(f'Bad type for {type(flds)=}')
+    code = flds.get(CODE)
+    country_code = flds.get(COUNTRY_CODE)
+    if not flds.get(NAME):
+        raise ValueError(f'Bad value for {flds.get(NAME)=}')
+    if not code:
+        raise ValueError(f'Bad value for {code=}')
+    if not country_code:
+        raise ValueError(f'Bad value for {country_code=}')
+    if (code, country_code) in cache:
+        raise ValueError(f'Duplicate key: {code=}; {country_code=}')
+    new_id = dbc.create(STATE_COLLECTION, flds)
+    print(f'{new_id=}')
+    load_cache()
+    return new_id
+
+
+def delete(code: str, cntry_code: str) -> bool:
+    ret = dbc.delete(STATE_COLLECTION, {CODE: code, COUNTRY_CODE: cntry_code})
+    if ret < 1:
+        raise ValueError(f'State not found: {code}, {cntry_code}')
+    load_cache()
+    return ret
+
+
+@needs_cache
+def read() -> dict:
+    return cache
+
+
+def load_cache():
+    global cache
+    cache = {}
+    states = dbc.read(STATE_COLLECTION)
+    for state in states:
+        cache[(state[CODE], state[COUNTRY_CODE])] = state
+
+
+def main():
+    create(SAMPLE_STATE)
+    print(read())
+
+
+if __name__ == '__main__':
+    main()
---
+++ states/tests/test_queries.py
@@ -0,0 +1,84 @@
+from copy import deepcopy
+
+from unittest.mock import patch
+import pytest
+
+import states.queries as qry
+
+
+def get_temp_rec():
+    return deepcopy(qry.SAMPLE_STATE)
+
+
+@pytest.fixture(scope='function')
+def temp_state_no_del():
+    temp_rec = get_temp_rec()
+    qry.create(get_temp_rec())
+    return temp_rec
+
+
+@pytest.fixture(scope='function')
+def temp_state():
+    temp_rec = get_temp_rec()
+    new_rec_id = qry.create(get_temp_rec())
+    yield new_rec_id
+    try:
+        qry.delete(temp_rec[qry.CODE], temp_rec[qry.COUNTRY_CODE])
+    except ValueError:
+        print('The record was already deleted.')
+
+
+@pytest.mark.skip('This is an example of a bad test!')
+def test_bad_test_for_count():
+    assert qry.count() == len(qry.cache)
+
+
+def test_count():
+    # get the count
+    old_count = qry.count()
+    # add a record
+    qry.create(get_temp_rec())
+    assert qry.count() == old_count + 1
+    qry.delete(qry.SAMPLE_CODE, qry.SAMPLE_COUNTRY)
+
+
+def test_good_create():
+    old_count = qry.count()
+    new_rec_id = qry.create(get_temp_rec())
+    assert qry.is_valid_id(new_rec_id)
+    assert qry.count() == old_count + 1
+    qry.delete(qry.SAMPLE_CODE, qry.SAMPLE_COUNTRY)
+
+
+def test_create_dup_key():
+    qry.create(get_temp_rec())
+    with pytest.raises(ValueError):
+        qry.create(get_temp_rec())
+    qry.delete(qry.SAMPLE_CODE, qry.SAMPLE_COUNTRY)
+
+
+def test_create_bad_name():
+    with pytest.raises(ValueError):
+        qry.create({})
+
+
+def test_create_bad_param_type():
+    with pytest.raises(ValueError):
+        qry.create(17)
+
+
+def test_delete(temp_state_no_del):
+    ret = qry.delete(temp_state_no_del[qry.CODE],
+                     temp_state_no_del[qry.COUNTRY_CODE])
+    assert ret == 1
+
+
+def test_delete_not_there():
+    with pytest.raises(ValueError):
+        qry.delete('some state code that is not there', 'not a country code')
+
+
+def test_read(temp_state):
+    states = qry.read()
+    assert isinstance(states, dict)
+    assert qry.SAMPLE_KEY in states
