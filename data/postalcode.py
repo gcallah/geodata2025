67959383e97662a07e85efb1b0a83d9d57ae49a4
gcallah <gcallah@mac.com>
2026-04-02 11:06:43 -0400

    Working on postal code.

--- data/postalcode.py
+++ data/postalcode.py
@@ -7,9 +7,19 @@ class PostalCode(ABC):
         print("Can't init this class!")
 
 
+US_POSTCODE_LEN = 5  # maybe handle 9-char codes later?
+
+
 class USPostalCode(PostalCode):
     def __init__(self, code: str):
+        if not isinstance(code, str):
+            raise TypeError(f'Bad type for code: {type(code)}')
         self.code = code
 
     def get_code(self) -> str:
         return self.code
+
+
+
+def main():
+    us_code = USPostalCode('khjfjhk')
