a20afc0d1419f86ca357609645ea180a9f62a0be
gcallah <gcallah@mac.com>
2026-04-02 12:15:45 -0400

    Adding more input checking to US postal code.

--- data/postalcode.py
+++ data/postalcode.py
@@ -14,12 +14,23 @@ class USPostalCode(PostalCode):
     def __init__(self, code: str):
         if not isinstance(code, str):
             raise TypeError(f'Bad type for code: {type(code)}')
+        if len(code) != US_POSTCODE_LEN:
+            raise ValueError(f'Bad value for {code=}')
+        if not code.isdigit():
+            raise ValueError(f'Only numbers allowed in {code=}')
         self.code = code
+
+    def __str__(self):
+        return self.code
 
     def get_code(self) -> str:
         return self.code
 
 
-
 def main():
-    us_code = USPostalCode('khjfjhk')
+    us_code = USPostalCode('khjfj')
+    print(us_code)
+
+
+if __name__ == '__main__':
+    main()
--- data/tests/test_postalcode.py
+++ data/tests/test_postalcode.py
@@ -13,3 +13,18 @@ def test_abc_base():
 def test_construct_us_code():
     us_code = pc.USPostalCode(TEST_CODE)
     assert isinstance(us_code, pc.USPostalCode)
+
+
+def test_construct_us_code_bad_type():
+    with pytest.raises(TypeError):
+        pc.USPostalCode(42)
+
+
+def test_construct_us_code_bad_length():
+    with pytest.raises(ValueError):
+        pc.USPostalCode('kjshfjkshdjfkhdjkfhdjkhfjksdhfjk')
+
+
+def test_construct_us_code_bad_chars():
+    with pytest.raises(ValueError):
+        pc.USPostalCode('k' * pc.US_POSTCODE_LEN)
